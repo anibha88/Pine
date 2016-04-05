6b28504b3588e9d714fc5228c152c8479ce17f90
anibha88 <anibha88@gmail.com>
2016-04-05 18:30:26 +0530

    some more iterations

--- simple_block_call.rb
+++ simple_block_call.rb
@@ -5,3 +5,17 @@ end
 profile do 
   p "hi"
 end
+
+# def profile &block
+#   block.call
+# end
+
+# profile do 
+#   p "hi"
+#   10.times do 
+#     p "HSVJ"
+#   end
+#   profile do
+#     p "Test"
+#   end
+# end
